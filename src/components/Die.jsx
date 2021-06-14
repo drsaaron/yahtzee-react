752f3dad0b4ecdbdfa21cb3943e10dc1c9d88418
Scott Aaron <dr_saaron@yahoo.com>
2021-06-14 16:43:38 -0500

    allow clicking on the die itself to toggle keeper

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -10,11 +10,18 @@ export default class Die extends Component {
 	this.state = { keeper: this.props.die.keeper };
 
 	this.handleKeeperChange = this.handleKeeperChange.bind(this);
+	this.handleImageClick = this.handleImageClick.bind(this);
     }
 
     handleKeeperChange(event) {
 	this.setState({...this.state, keeper: event.target.checked });
 	this.props.toggleDieKeeper(this.props.die, event.target.checked);
+    }
+
+    handleImageClick(event) {
+	var checked = !this.state.keeper;
+	this.setState({...this.state, keeper: checked });
+	this.props.toggleDieKeeper(this.props.die, checked);
     }
     
     render() {
@@ -26,10 +33,10 @@ export default class Die extends Component {
 	    <div className="die">
 		<form>
 		    <div className="displayVertical">
-			<img src={imageUrl} alt={die.value} className="dieImage" />
+			<img src={imageUrl} alt={die.value} className="dieImage" onClick={this.handleImageClick} />
 		    </div>
 		    <div className="displayVertical">
-		        <input type="checkbox" onChange={this.handleKeeperChange} />
+		        <input type="checkbox" onChange={this.handleKeeperChange} checked={this.props.die.keeper} />
 		    </div>
 		</form>
 	    </div>
