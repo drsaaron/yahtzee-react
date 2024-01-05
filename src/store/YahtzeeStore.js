2cd46a29a1ff73380f8005ab17a060010b08139c
Scott Aaron <dr_saaron@yahoo.com>
2024-01-05 11:28:17 -0600

    version update

--- src/store/YahtzeeStore.js
+++ src/store/YahtzeeStore.js
@@ -1,7 +1,7 @@
 
 import {createStore, applyMiddleware} from 'redux';
 import createRootReducer from '../reducers/rootReducer';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import logger from 'redux-logger';
 
 const store = createStore(createRootReducer(), {}, applyMiddleware(thunk, logger));
