a876718601fa2fad6c96ee63269ecfb42431bda8
Scott Aaron <dr_saaron@yahoo.com>
2025-04-18 10:30:48 -0500

    express 5

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.post('/api/highScore', (req, res) => {
 });
 
 // if all else fails, return the react app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
