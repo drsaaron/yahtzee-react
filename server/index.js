32b199bf43cfbe2b080be2e0bc0aa4f5770255b9
Scott Aaron <dr_saaron@yahoo.com>
2025-04-20 17:36:25 -0500

    remove the general get completely

--- server/index.js
+++ server/index.js
@@ -34,10 +34,6 @@ app.post('/api/highScore', (req, res) => {
 	});
 });
 
-// if all else fails, return the react app
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-});
 
 const PORT = config.get('PORT');
 httpServer.listen(PORT, () => { console.log(`Application server listening on port ${PORT} (http)`); });
