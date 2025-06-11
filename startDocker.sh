0e3b6dc0497de804490f0cb6f988fbccd359878f
Scott Aaron <dr_saaron@yahoo.com>
2025-06-11 10:18:01 -0500

    version update

--- startDocker.sh
+++ startDocker.sh
@@ -9,5 +9,11 @@ containerName=yahtzee
 docker stop $containerName
 docker rm $containerName
 
-docker run --network qotd --user $(id -u):$(id -g) --name $containerName -d -p 8880:$port  $imageName:$version
+# build the mongo DB URL
+mongoUser=myUser
+mongoPass=$(pass Database/mongo/atlas/$mongoUser | head -1)
+mongoServer=$(pass Database/mongo/atlas/$mongoUser | tail -1 | awk -F/ '{ print $3 }')
+SCORE_DB_URL="mongodb+srv://$mongoUser:$mongoPass@$mongoServer/yahtzee"
+
+docker run --network qotd --user $(id -u):$(id -g) --name $containerName -d -p 8880:$port -e SCORE_DB_URL="$SCORE_DB_URL" $imageName:$version
 
